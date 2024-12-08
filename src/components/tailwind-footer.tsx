bf54f608325d4f56d06d2779aa5e7b33c6178a38
KimPuro <puro@protonmail.ch>
2024-12-09 05:29:16 +0900

    feat: footer에 kt 로고 추가

--- src/components/tailwind-footer.tsx
+++ src/components/tailwind-footer.tsx
@@ -89,12 +89,12 @@ const navigation = {
 
 export default function TailwindFooter() {
   return (
-    <footer className="bg-white">
+    <footer className="w-full bg-white">
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
           <img
-            alt="Company name"
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+            alt="img-logo-kr"
+            src="/images/img-logo-kr.svg"
             className="h-9"
           />
           <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
@@ -170,39 +170,6 @@ export default function TailwindFooter() {
             </div>
           </div>
         </div>
-        <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between">
-          <div>
-            <h3 className="text-sm/6 font-semibold text-gray-900">
-              Subscribe to our newsletter
-            </h3>
-            <p className="mt-2 text-sm/6 text-gray-600">
-              The latest news, articles, and resources, sent to your inbox
-              weekly.
-            </p>
-          </div>
-          <form className="mt-6 sm:flex sm:max-w-md lg:mt-0">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email-address"
-              type="email"
-              required
-              placeholder="Enter your email"
-              autoComplete="email"
-              className="w-full min-w-0 rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:w-56 sm:text-sm/6"
-            />
-            <div className="mt-4 sm:ml-4 sm:mt-0 sm:shrink-0">
-              <button
-                type="submit"
-                className="flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Subscribe
-              </button>
-            </div>
-          </form>
-        </div>
         <div className="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
           <div className="flex gap-x-6 md:order-2">
             {navigation.social.map((item) => (
@@ -217,7 +184,7 @@ export default function TailwindFooter() {
             ))}
           </div>
           <p className="mt-8 text-sm/6 text-gray-600 md:order-1 md:mt-0">
-            &copy; 2024 Your Company, Inc. All rights reserved.
+            &copy; Copyright 2022 kt sports. All rights reserved.
           </p>
         </div>
       </div>
