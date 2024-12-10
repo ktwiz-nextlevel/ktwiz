bddfe3302e842608bc6f1cc7a634791b4680510a
jio <jiographie@gmail.com>
2024-12-10 18:25:01 +0900

    feat: 검색바 기능 구현 ref: #28

--- src/components/common/search-bar.tsx
+++ src/components/common/search-bar.tsx
@@ -1,12 +1,30 @@
-function SearchBar() {
+'use client'
+import { useState } from 'react'
+
+interface SearchBarProps {
+  onSearch: (query: string) => void
+}
+
+function SearchBar({ onSearch }: SearchBarProps) {
+  const [query, setQuery] = useState('')
+
+  const handleSearch = () => {
+    onSearch(query)
+  }
+
   return (
     <div className="flex gap-[5px] align-middle text-[12px]">
       <input
         type="text"
         className="h-[30px] w-[220px] border border-[#9a9a9a] px-[8px]"
         placeholder="검색어를 입력해주세요"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
-      <button className="h-[30px] rounded-none bg-[#333] py-0 text-white">
+      <button
+        className="h-[30px] rounded-none bg-[#333] py-0 text-white"
+        onClick={handleSearch}
+      >
         검색
       </button>
     </div>
