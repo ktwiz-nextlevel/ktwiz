21d22288afc840d724c2781693d1204f2cc90a10
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-12-11 02:36:52 +0900

    chore: 기타

--- src/app/game/regular/schedule/page.tsx
+++ src/app/game/regular/schedule/page.tsx
@@ -3,6 +3,7 @@ import TabMenu from '@/components/common/tab-menu'
 import { GAME_BANNER_DATA } from '@/contants/index'
 
 function SchedulePage({ params }: { params: { route: string } }) {
+  console.log(params)
   return (
     <div className="w-full">
       <Banner {...GAME_BANNER_DATA['/regular']}>
