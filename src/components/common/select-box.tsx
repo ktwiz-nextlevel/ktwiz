260540c1ad75c14f89299aa97341abe284b86e38
KimPuro <puro@protonmail.ch>
2024-12-11 00:43:35 +0900

    feat: select-box 컴포넌트에 상태 및 JSDoc 주석 추가
    ref: #26

--- src/components/common/select-box.tsx
+++ src/components/common/select-box.tsx
@@ -11,27 +11,38 @@ import {
 import { ChevronUpDownIcon } from '@heroicons/react/16/solid'
 import { CheckIcon } from '@heroicons/react/20/solid'
 
-const people = [
-  { id: 1, name: 'Wade Cooper' },
-  { id: 2, name: 'Arlene Mccoy' },
-  { id: 3, name: 'Devon Webb' },
-  { id: 4, name: 'Tom Cook' },
-  { id: 5, name: 'Tanya Fox' },
-  { id: 6, name: 'Hellen Schmidt' },
-  { id: 7, name: 'Caroline Schultz' },
-  { id: 8, name: 'Mason Heaney' },
-  { id: 9, name: 'Claudie Smitham' },
-  { id: 10, name: 'Emil Schaefer' },
-]
+type OptionType = {
+  id: number
+  name: string
+}
 
-export default function SimpleCustom() {
-  const [selected, setSelected] = useState(people[3])
+/**
+ * SelectBox 컴포넌트 입니다.
+ * 사용 방법은 같이 있는 SelectBoxExample 컴포넌트를 참고해주세요.
+ * @param option - id와 name을 가진 객체 배열
+ * @param onChange - 선택된 option을 인자로 받는 콜백 함수
+ */
+
+//  상태 관리에 관한 부분은 협의가 필요
+export default function SelectBox({
+  option,
+  onChange,
+}: {
+  option: OptionType[]
+  onChange?: (selected: OptionType) => void
+}) {
+  const [selected, setSelected] = useState(option[0])
+
+  const handleChange = (value: OptionType) => {
+    setSelected(value)
+    if (onChange) {
+      onChange(value)
+    }
+  }
 
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      <Label className="block text-sm/6 font-medium text-gray-900">
-        Assigned to
-      </Label>
+    <Listbox value={selected} onChange={handleChange}>
+      {/* width 크기는 여기 div를 조절 */}
       <div className="relative mt-2">
         <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pl-3 pr-2 text-left text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
           <span className="col-start-1 row-start-1 truncate pr-6">
@@ -47,14 +58,14 @@ export default function SimpleCustom() {
           transition
           className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
         >
-          {people.map((person) => (
+          {option.map((options) => (
             <ListboxOption
-              key={person.id}
-              value={person}
+              key={options.id}
+              value={options}
               className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white data-[focus]:outline-none"
             >
               <span className="block truncate font-normal group-data-[selected]:font-semibold">
-                {person.name}
+                {options.name}
               </span>
 
               <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-[&:not([data-selected])]:hidden group-data-[focus]:text-white">
