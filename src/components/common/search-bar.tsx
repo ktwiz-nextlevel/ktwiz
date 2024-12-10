f015558b596836daa8f884c567e1b3260ec5472a
jio <jiographie@gmail.com>
2024-12-11 01:40:11 +0900

    feat: 키보드 이벤트 및 유효성 검사 추가 ref: #28

--- src/components/common/search-bar.tsx
+++ src/components/common/search-bar.tsx
@@ -8,25 +8,27 @@ interface SearchBarProps {
 function SearchBar({ onSearch }: SearchBarProps) {
   const [query, setQuery] = useState('')
 
-  const handleSearch = () => {
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const trimmedQuery = query.trim()
+    if (!trimmedQuery) return
     onSearch(query)
   }
 
   return (
     <div className="flex gap-[5px] align-middle text-[12px]">
-      <input
-        type="text"
-        className="h-[30px] w-[220px] border border-[#9a9a9a] px-[8px]"
-        placeholder="검색어를 입력해주세요"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button
-        className="h-[30px] rounded-none bg-[#333] py-0 text-white"
-        onClick={handleSearch}
-      >
-        검색
-      </button>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          className="h-[30px] w-[220px] border border-[#9a9a9a] px-[8px]"
+          placeholder="검색어를 입력해주세요"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="h-[30px] rounded-none bg-[#333] py-0 text-white">
+          검색
+        </button>
+      </form>
     </div>
   )
 }
