03934397ff2cbcad16ab7860240c20eb6d2fc7df
HyoHwan <gyghks1017@gmail.com>
2024-12-13 01:42:50 +0900

    chore: player page 셋팅

---
+++ src/app/player/catcher/page.tsx
@@ -0,0 +1,7 @@
+export default function catcher() {
+  return (
+    <div>
+      <div>타자</div>
+    </div>
+  )
+}
---
+++ src/app/player/pitcher/page.tsx
@@ -0,0 +1,9 @@
+//투수 페이지
+
+export default function pitcher() {
+  return (
+    <>
+      <div>투수</div>
+    </>
+  )
+}
