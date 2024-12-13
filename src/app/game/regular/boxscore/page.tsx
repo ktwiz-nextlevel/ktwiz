458b956d6c20dcba7cca2614730c867340d179b9
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-12-13 16:33:30 +0900

    style: page css 변경

--- src/app/game/regular/boxscore/page.tsx
+++ src/app/game/regular/boxscore/page.tsx
@@ -1,15 +1,22 @@
 import { Banner } from '@/components/common/banner'
 import TabMenu from '@/components/common/tab-menu'
+import Breadcrumbs from '@/components/tailwind-ui/breadcrumbs/simple-with-chevrons'
+
 import { GAME_BANNER_DATA } from '@/contants/index'
 
 function BoxscorePage({ params }: { params: { id: string } }) {
-  console.log(params)
+  const pages = ['HOME', '정규리그', '박스스코어']
   return (
     <div className="w-full">
       <Banner {...GAME_BANNER_DATA['/regular']}>
         <TabMenu tabs={GAME_BANNER_DATA['/regular'].tabs} />
       </Banner>
-      <div className="page">BoxscorePage</div>
+      <div className="page border border-red-400">
+        <div className="mt-[50px] flex w-full justify-end">
+          <Breadcrumbs pages={pages} />
+        </div>
+        <section className="mt-6 border border-red-500"></section>
+      </div>
     </div>
   )
 }
