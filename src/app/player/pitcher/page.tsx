a08fab4783cf9de524d94dfed22bc1d638b023ec
HyoHwan <gyghks1017@gmail.com>
2024-12-13 18:32:36 +0900

    chore: dev 브랜치 pull 받기전 저장

--- src/app/player/pitcher/page.tsx
+++ src/app/player/pitcher/page.tsx
@@ -1,9 +1,18 @@
-//투수 페이지
+import Image from 'next/image'
 
-export default function pitcher() {
+export default function Pitcher() {
   return (
     <>
-      <div>투수</div>
+      <div className="">
+        <Image
+          className="object-cover"
+          src="/images/player-test-img.jpg"
+          alt="Profile Image"
+          width={100} // 너비를 기준으로 비율 유지
+          height={150} // 높이는 자동 계산
+          layout="intrinsic" // 원본 비율 유지
+        />
+      </div>
     </>
   )
 }
