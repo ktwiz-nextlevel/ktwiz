b178b7c451368d0dd8564388261509df1b1ce92e
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-12-14 23:30:19 +0900

    feat: scoreBoard Header 구현

--- src/app/game/regular/boxscore/page.tsx
+++ src/app/game/regular/boxscore/page.tsx
@@ -3,14 +3,20 @@ import TabMenu from '@/components/common/tab-menu'
 import Breadcrumbs from '@/components/tailwind-ui/breadcrumbs/simple-with-chevrons'
 
 import { GAME_BANNER_DATA } from '@/contants/index'
+import { GameSchedule } from '@/types/game'
 
-function BoxscorePage({ params }: { params: { id: string } }) {
+async function BoxscorePage({ params }: { params: { id: string } }) {
+  const res = await fetch(
+    'http://54.180.229.183/api/game/boxscore?gameDate=20240910&gmkey=20240910NCKT0',
+  )
+  const data = await res.json()
+
   return (
     <div className="w-full">
       <Banner {...GAME_BANNER_DATA['/regular']}>
         <TabMenu tabs={GAME_BANNER_DATA['/regular'].tabs} />
       </Banner>
-      <div className="page border border-gray-400">
+      <div className="page">
         <div className="mt-[50px] flex w-full justify-end">
           <Breadcrumbs pages={['HOME', '정규리그', '박스스코어']} />
         </div>
@@ -21,55 +27,96 @@ function BoxscorePage({ params }: { params: { id: string } }) {
   )
 }
 
-function BoxscoreBoardHeader() {
-  // const
+export default BoxscorePage
+
+export function ScoreBoard() {
   return (
-    <div className="flex justify-center gap-5">
-      {/* 왼쪽 구단 */}
-      <div className="flex items-center gap-1 pt-5">
-        <div className="flex flex-col justify-end">
-          <p className="flex gap-1">
-            {/* <span>홈</span> */}
-            <h1 className="text-base">두산</h1>
-          </p>
-          <span className="text-right text-sm">패</span>
+    <div className="mt-5 overflow-hidden rounded-md border border-gray-200 bg-[--red-color-300]">
+      <ul role="list" className="divide-y divide-gray-200">
+        <li className="px-6 py-4">
+          <BoxscoreBoardHeader />
+        </li>
+        <li className="px-6 py-4">{/* Your content */}</li>
+        <li className="px-6 py-4">{/* Your content */}</li>
+      </ul>
+    </div>
+  )
+}
+async function BoxscoreBoardHeader() {
+  const res = await fetch(
+    'http://54.180.229.183/api/game/boxscore?gameDate=20240910&gmkey=20240910NCKT0',
+  )
+  const data = await res.json()
+  const {
+    current,
+    prev,
+    next,
+  }: { current: GameSchedule; prev: GameSchedule; next: GameSchedule } =
+    data.data.schedule
+  console.log(current)
+  return (
+    <div className="flex w-full flex-col items-center">
+      <Label status={current.game} />
+      <h1 className="mt-2 text-xl text-gray-600">
+        {`${current.gyear}년 ${current.gmonth}월 ${current.gday}일`}
+      </h1>
+      <p className="mt-0 text-xs text-gray-400">{`${current.gmonth}.${current.gday} ${current.gtime} | ${current.stadium}`}</p>
+      <div className="mt-2 flex justify-center gap-5">
+        {/* 왼쪽 구단 */}
+        <div className="flex w-[150px] items-center justify-end gap-1">
+          <div className="flex flex-col justify-end">
+            <span className="align-top text-base">{current.visit}</span>
+            {current.endFlag && (
+              <WinLossStats isWin={current.vscore > current.hscore} />
+            )}
+          </div>
+          <img src={current.visitLogo} alt="visitLogo" className="w-16" />
         </div>
-        <img src="/images/team-logo.png" alt="dfdf" className="" />
-        <h2 className="text-6xl font-extrabold">3</h2>
-      </div>
-      {/* 경기정보 */}
-      <div className="flex flex-col items-center">
-        <Label />
-        <h1 className="mt-1 text-2xl font-bold text-gray-500">
-          2024년 10월 11일
-        </h1>
-        <p className="mt-0 text-sm text-gray-400">10.01 17:00 | 수원</p>
-        {/* <p className="text-gray-500">수원</p> */}
-      </div>
-      {/* 오른쪽 구단 */}
-      <div className="flex items-center gap-1 pt-4">
-        <h2 className="text-6xl font-extrabold">2</h2>
-        <img src="/images/team-logo.png" alt="dfdf" className="" />
-        <div className="flex flex-col justify-start">
-          <p className="flex gap-1">
-            <HomeLabel />
-            <h1 className="text-base">키움</h1>
-          </p>
-          <span className="text-left text-sm">승</span>
+        {/* 경기정보 */}
+        <div className="flex items-center justify-center">
+          <h2
+            className={`align-top text-5xl font-extrabold ${current.vscore > current.hscore ? 'text-[--main-red-color]' : 'text-gray-500'}`}
+          >
+            {current.vscore}
+          </h2>
+          <h2 className="mt-[-5px] align-top text-5xl font-extrabold text-gray-500">
+            &nbsp;:&nbsp;
+          </h2>
+          <h2
+            className={`align-top text-5xl font-extrabold ${current.vscore < current.hscore ? 'text-[--main-red-color]' : 'text-gray-500'}`}
+          >
+            {current.hscore}
+          </h2>
+        </div>
+        {/* 오른쪽 구단 */}
+        <div className="flex w-[150px] items-center justify-start gap-1">
+          <img src={current.homeLogo} alt="homeLogo" className="w-16" />
+          <div className="flex flex-col justify-start">
+            <div className="flex gap-1">
+              <HomeLabel />
+              <span className="text-base">{current.home}</span>
+            </div>
+            {current.endFlag && (
+              <WinLossStats
+                isWin={current.vscore < current.hscore}
+                style="left"
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
   )
 }
-function Label({ status = 'prev' }: { status?: 'prev' | 'active' | 'done' }) {
+function Label({ status = 'prev' }: { status?: 'prev' | 'current' | 'next' }) {
   if (status === 'prev') {
     return (
-      <span className="rounded-xl bg-gray-100 px-3 py-1 text-xs text-gray-500">
+      <span className="rounded-xl bg-gray-200 px-3 py-1 text-xs text-gray-500">
         경기전
       </span>
     )
   }
-  if (status === 'active') {
+  if (status === 'current') {
     return (
       <span className="rounded-xl bg-[--main-red-color] px-3 py-1 text-xs text-white">
         경기중
@@ -89,18 +136,28 @@ function HomeLabel() {
     </span>
   )
 }
-export default BoxscorePage
 
-export function ScoreBoard() {
+function WinLossStats({
+  isWin,
+  style = 'right',
+}: {
+  isWin: boolean
+  style?: 'left' | 'right'
+}) {
+  if (isWin)
+    return (
+      <span
+        className={`${style === 'left' ? 'text-left' : 'text-right'} text-sm text-[--main-red-color]`}
+      >
+        승
+      </span>
+    )
+
   return (
-    <div className="mt-5 overflow-hidden rounded-md border border-gray-300 bg-white">
-      <ul role="list" className="divide-y divide-gray-300">
-        <li className="px-6 py-4">
-          <BoxscoreBoardHeader />
-        </li>
-        <li className="px-6 py-4">{/* Your content */}</li>
-        <li className="px-6 py-4">{/* Your content */}</li>
-      </ul>
-    </div>
+    <span
+      className={`${style === 'left' ? 'text-left' : 'text-right'} text-right text-sm text-gray-500`}
+    >
+      패
+    </span>
   )
 }
---
+++ src/types/game.ts
@@ -0,0 +1,23 @@
+export interface GameSchedule {
+  broadcast: string // 방송사
+  cancelFlag: '0' | '1' // 경기 취소 여부 ('0': 취소 안됨, '1': 취소됨)
+  crowdCn: number // 관중 수
+  endFlag: '0' | '1' // 경기 종료 여부 ('0': 진행 중, '1': 종료됨)
+  game: 'current' | 'prev' | 'next' // 경기 상태
+  gameDate: number // 경기 날짜 (YYYYMMDD 형식)
+  gday: number // 경기 일 (일자)
+  gmkey: string // 경기 고유 키
+  gmonth: number // 경기 월
+  gtime: string // 경기 시간 (HH:mm 형식)
+  gyear: string // 경기 연도
+  home: string // 홈팀 이름
+  homeKey: string // 홈팀 키
+  homeLogo: string // 홈팀 로고 URL
+  hscore: number // 홈팀 점수
+  stadium: string // 경기장 이름
+  stadiumKey: string // 경기장 키
+  visit: string // 원정팀 이름
+  visitKey: string // 원정팀 키
+  visitLogo: string // 원정팀 로고 URL
+  vscore: number // 원정팀 점수
+}
