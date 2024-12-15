1a2e84d9e805db77c9f3e5f2333c51264fee9c2e
Eunhee <82435813+joywhy@users.noreply.github.com>
2024-12-15 18:13:44 +0900

    refactor: types 폴더 index.ts import * 로 변경

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,3 @@
-export type { BannerData, TabsType, BannerInterface } from './banner'
-export type { GameSchedule, ScheduleType } from './game'
-export type { MenuData, LnbDataArray } from './header'
+export type * from './banner'
+export type * from './game'
+export type * from './header'
