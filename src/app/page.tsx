186576c6629286e4b2fe3add4d3b840bd1f28f2b
jio <jiographie@gmail.com>
2024-12-15 18:55:58 +0900

    chore: supabase 셋팅 ssr 추가 설정 ref: #58

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import LoginModal from '@/components/modal/login'
+
 export default function Page() {
-  return <div className="text-[--blue-color-100]">dfd</div>
+  return (
+    <div className="">
+      dfd
+      <LoginModal />
+    </div>
+  )
 }
