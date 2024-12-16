5ca0682678468ae70a1e7f1436a77f41aa184023
HyoHwan <gyghks1017@gmail.com>
2024-12-17 02:18:07 +0900

    feat: recharts 이용해서 차트 랜더링
    recharts 기본셋팅

--- src/app/player/pitcher/page.tsx
+++ src/app/player/pitcher/page.tsx
@@ -1,15 +1,22 @@
 import PlayerCardList from '@/components/player/player-card-list'
 import PlayerDetail from '@/components/player/player-detail'
+import PlayerChartData from '@/components/player/PlayerChartData'
 
 export default function Pitcher() {
   return (
-    <div className="page-large mx-auto flex max-w-6xl flex-col p-4 md:flex-row">
-      <div className="w-auto flex-shrink-0 p-2">
-        <PlayerCardList />
+    <div className="page-large mx-auto flex max-w-6xl flex-col p-4">
+      <div className="flex w-full flex-col md:flex-row">
+        <div className="w-auto flex-shrink-0 p-2">
+          <PlayerCardList />
+        </div>
+
+        <div className="w-full p-2">
+          <PlayerDetail />
+        </div>
       </div>
 
       <div className="w-full p-2">
-        <PlayerDetail />
+        <PlayerChartData />
       </div>
     </div>
   )
