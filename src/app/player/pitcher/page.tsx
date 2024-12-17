84251045b07f9930f09dc68ae0847d7d64568f98
HyoHwan <gyghks1017@gmail.com>
2024-12-17 16:38:28 +0900

    refactor: 핸들러 함수 삭제하고 상태값으로 바로 전달

--- src/app/player/pitcher/page.tsx
+++ src/app/player/pitcher/page.tsx
@@ -7,16 +7,12 @@ import { useState } from 'react'
 
 export default function Pitcher() {
   const [playerId, setPlayerId] = useState(1)
-  const handleCardClick = (id) => {
-    console.log(`상위 컴포넌트에서 받은 아이디: ${id}`)
-    setPlayerId(id)
-  }
 
   return (
     <div className="page-large mx-auto flex max-w-6xl flex-col p-4">
       <div className="flex w-full flex-col md:flex-row">
         <div className="w-auto flex-shrink-0 p-2">
-          <PlayerCardList onCardClick={handleCardClick} />
+          <PlayerCardList onCardClick={setPlayerId} />
         </div>
 
         <div className="w-full p-2">
