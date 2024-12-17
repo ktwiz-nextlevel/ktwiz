91afd9c4b311d1418d999390ab66d4d5439993ff
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-18 03:28:11 +0900

    feat: liveTalk 숨기기 기능 추가

--- src/app/fan/board/page.tsx
+++ src/app/fan/board/page.tsx
@@ -1,5 +1,6 @@
 import BoardSearchBar from '@/components/board/board-search-bar'
 import LiveTalk from '@/components/board/live-talk'
+import LiveTalkToggle from '@/components/board/live-talk-toggle'
 import { CreatePost } from '@/components/board/post-buttons'
 import PostCard from '@/components/board/post-card'
 import { Banner } from '@/components/common/banner'
@@ -35,8 +36,8 @@ export default async function FanBoardPage({
       <Banner {...FAN_BANNER_DATA['/']}>
         <TabMenu tabs={FAN_BANNER_DATA['/'].tabs} />
       </Banner>
-      <div className="flex w-full justify-center space-x-12 px-4 pb-16">
-        <div className="w-[1100px]">
+      <div className="flex w-full space-x-4 px-10 pb-16 lg:pl-20 xl:pl-52">
+        <div className="max-w-[1100px] flex-1">
           <div className="mt-[50px] flex w-full justify-between">
             <BoardSearchBar />
             <Breadcrumbs pages={['HOME', 'FAN', '팬 소통공간']} />
@@ -49,7 +50,7 @@ export default async function FanBoardPage({
             <CreatePost />
           </div>
         </div>
-        <LiveTalk userData={userData} />
+        <LiveTalkToggle userData={userData} />
       </div>
     </div>
   )
