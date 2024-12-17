000337f5600e82f24806817183d6df2bafbedae1
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-17 17:54:41 +0900

    style: 작성하기 버튼 색상 변경

--- src/components/board/post-buttons.tsx
+++ src/components/board/post-buttons.tsx
@@ -4,7 +4,7 @@ export function CreatePost() {
   return (
     <Link
       href="/fan/board/write"
-      className="flex h-10 items-center rounded-lg bg-gray-600 px-4 text-sm text-white"
+      className="flex h-10 items-center rounded-lg bg-[--black-color-500] px-4 text-sm text-white"
     >
       <span className="hidden md:block">작성하기</span>{' '}
     </Link>
