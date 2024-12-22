1185b06842853562f4a3f6786b825511d16042db
jio <jiographie@gmail.com>
2024-12-22 16:51:48 +0900

    style: 경기일정 카드 컴포넌트 제작 ref: #66

--- src/app/game/regular/schedule/page.tsx
+++ src/app/game/regular/schedule/page.tsx
@@ -1,5 +1,15 @@
+import ScoreCardGroup from '@/components/schedule/score-card-group'
+import Breadcrumbs from '@/components/tailwind-ui/breadcrumbs/simple-with-chevrons'
+
 function SchedulePage() {
-  return <div className="w-full">SchedulePage</div>
+  return (
+    <div className="w-full">
+      <div className="my-[50px] flex w-full justify-end">
+        <Breadcrumbs pages={['HOME', '정규리그', '경기일정']} />
+      </div>
+      <ScoreCardGroup />
+    </div>
+  )
 }
 
 export default SchedulePage
