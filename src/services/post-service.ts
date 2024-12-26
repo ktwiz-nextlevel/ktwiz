1c8a34487af2775ce10c95411dfbe9bb930f78c4
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-27 03:56:37 +0900

    feat: 게시글 상세 데이터 fetching 함수 및 댓글 paginaiton기능을 위한 함수 추가, 페이지별 아이템 수 상수화

--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -1,9 +1,11 @@
-import { PostListItem } from '@/types'
+import { PostDetail, PostListItem } from '@/types'
 import { createClient } from '@/utils/supabase/server'
+
+const POSTS_PER_PAGE = 10
+const COMMENTS_PER_PAGE = 5
 
 export async function fetchPostsPages(query: string): Promise<number | null> {
   try {
-    const limit = 10
     const supabase = await createClient()
     const { count, error } = await supabase
       .from('posts')
@@ -15,7 +17,7 @@ export async function fetchPostsPages(query: string): Promise<number | null> {
     if (!count) {
       return null
     }
-    const totalPages = Math.ceil(count / limit)
+    const totalPages = Math.ceil(count / POSTS_PER_PAGE)
     return totalPages
   } catch (error) {
     console.error(error)
@@ -29,8 +31,7 @@ export async function fetchFilteredPost(
   type: 'title' | 'content',
 ): Promise<PostListItem[] | null> {
   try {
-    const limit = 10
-    const offset = (currentPage - 1) * limit
+    const offset = (currentPage - 1) * POSTS_PER_PAGE
     const supabase = await createClient()
     const columnToSearch = type === 'title' ? 'title' : 'content'
     const { data, error } = await supabase
@@ -48,7 +49,7 @@ export async function fetchFilteredPost(
       )
       .ilike(columnToSearch, `%${query}%`)
       .order('created_at', { ascending: false })
-      .range(offset, offset + limit - 1)
+      .range(offset, offset + POSTS_PER_PAGE - 1)
 
     if (error) {
       throw new Error(`Error fetching posts: ${error.message}`)
@@ -74,6 +75,136 @@ export async function fetchFilteredPost(
     }))
 
     return clientData
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
+export async function fetchCommentsPages(
+  postId: number,
+): Promise<number | null> {
+  const supabase = await createClient()
+  try {
+    const { count, error } = await supabase
+      .from('comments')
+      .select('id', { count: 'exact', head: true })
+      .eq('post_id', postId)
+
+    if (error) {
+      throw new Error(`Error fetching comments: ${error.message}`)
+    }
+    if (!count) {
+      return null
+    }
+    const totalPages = Math.ceil(count / COMMENTS_PER_PAGE)
+    return totalPages
+  } catch (error) {
+    console.error('Unexpected error:', error)
+    return null
+  }
+}
+
+export async function fetchPostById(
+  postId: number,
+  currentPage: number,
+): Promise<PostDetail | null> {
+  try {
+    const supabase = await createClient()
+    const { data: postData, error: postError } = await supabase
+      .from('posts')
+      .select(
+        `
+        id,
+        user_id,
+        title,
+        content,
+        created_at,
+        view_count,
+        author,
+        images (
+          image_url
+        ),
+        comments (
+          id,
+          post_id,
+          user_id,
+          content,
+          created_at,
+          author
+        )
+      `,
+      )
+      .eq('id', postId)
+      .single()
+
+    if (postError) {
+      throw new Error(`Error fetching post: ${postError.message}`)
+    }
+
+    if (!postData) {
+      return null
+    }
+
+    const { error: viewError } = await supabase
+      .from('posts')
+      .update({ view_count: postData.view_count + 1 })
+      .eq('id', postId)
+
+    if (viewError) {
+      throw new Error(`Error updating view count: ${viewError.message}`)
+    }
+
+    const offset = (currentPage - 1) * COMMENTS_PER_PAGE
+    const { data: commentsData, error: commentsError } = await supabase
+      .from('comments')
+      .select(
+        `
+        id,
+        post_id,
+        user_id,
+        content,
+        created_at,
+        author
+      `,
+      )
+      .eq('post_id', postId)
+      .order('created_at', { ascending: false })
+      .range(offset, offset + COMMENTS_PER_PAGE - 1)
+
+    if (commentsError) {
+      throw new Error(`Error fetching comments: ${commentsError.message}`)
+    }
+
+    const clientProjectData: PostDetail = {
+      id: postData.id,
+      userId: postData.user_id,
+      title: postData.title,
+      content: postData.content,
+      createdAt: new Date(postData.created_at).toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      }),
+      viewCount: postData.view_count + 1,
+      author: postData.author,
+      images: postData.images.map((image) => image.image_url),
+      comments: commentsData.map((comment) => ({
+        id: comment.id,
+        postId: comment.post_id,
+        userId: comment.user_id,
+        content: comment.content,
+        createdAt: new Date(comment.created_at).toLocaleDateString('ko-KR', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
+        author: comment.author,
+      })),
+    }
+    return clientProjectData
   } catch (error) {
     console.error(error)
     return null
