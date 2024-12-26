07042a8b2d7dc0c3b0865beb6c0b99a45e5526a6
chldmsqls34 <chldmsqls34@gmail.com>
2024-12-27 04:22:36 +0900

    feat: 게시글 상세 페이지 ui 구현 및 에러 처리 페이지 추가

--- src/app/fan/board/write/page.tsx
+++ src/app/fan/board/write/page.tsx
@@ -6,7 +6,7 @@ export default async function BoardWritePage() {
     <div className="flex w-full space-x-8 px-10 pb-16">
       <div className="mx-auto max-w-[1100px] flex-1">
         <div className="mt-[50px] flex w-full justify-end">
-          <Breadcrumbs pages={['HOME', 'FAN', '팬 소통공간']} />
+          <Breadcrumbs pages={['HOME', 'FAN', '팬 소통공간', '작성하기']} />
         </div>
         <CreatePostForm />
       </div>
