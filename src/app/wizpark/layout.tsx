8ec232d2afc3be04f1796984c6714a7a8696ec0d
KimPuro <puro@protonmail.ch>
2024-12-29 16:28:23 +0900

    feat: wizpark 레이아웃 추가

---
+++ src/app/wizpark/layout.tsx
@@ -0,0 +1,26 @@
+import Banner from '@/components/common/banner/banner'
+import TabMenu from '@/components/common/tab-menu2'
+import { GAME_BANNER_DATA, WIZ_PARK_BANNER_DATA } from '@/contants'
+import React from 'react'
+import WizparkContainer from '@/components/wizpark/wizpark-container'
+
+function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div>
+      <Banner>
+        <Banner.Heading
+          title={WIZ_PARK_BANNER_DATA[''].title}
+          subtitle={WIZ_PARK_BANNER_DATA[''].description}
+        />
+        <TabMenu tabs={WIZ_PARK_BANNER_DATA[''].tabs} />
+      </Banner>
+      <WizparkContainer> {children}</WizparkContainer>
+    </div>
+  )
+}
+
+export default Layout
