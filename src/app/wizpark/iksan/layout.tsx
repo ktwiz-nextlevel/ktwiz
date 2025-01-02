328dbb79bbe581ef4d62ff14b448b38c8b1c5dda
KimPuro <puro@protonmail.ch>
2025-01-02 22:26:54 +0900

    feat: 익산야구장 페이지 구현

---
+++ src/app/wizpark/iksan/layout.tsx
@@ -0,0 +1,26 @@
+import Banner from '@/components/common/banner/banner'
+import TabMenu from '@/components/common/tab-menu/tab-menu'
+import { GAME_BANNER_DATA, WIZ_PARK_BANNER_DATA } from '@/contants'
+import React from 'react'
+import WizparkContainer from '@/components/wizpark/wizpark-container'
+
+function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div>
+      <Banner>
+        <Banner.Heading
+          title={WIZ_PARK_BANNER_DATA['/iksan'].title}
+          subtitle={WIZ_PARK_BANNER_DATA['/iksan'].description}
+        />
+        <TabMenu tabs={WIZ_PARK_BANNER_DATA['/iksan'].tabs} />
+      </Banner>
+      <WizparkContainer> {children}</WizparkContainer>
+    </div>
+  )
+}
+
+export default Layout
