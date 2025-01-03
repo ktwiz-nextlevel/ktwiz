847edd7dfed89f2ecb498a0cc141a92dbaba72e0
chldmsqls34 <chldmsqls34@gmail.com>
2025-01-03 09:33:42 +0900

    refactor: 게시판 검색바 컴포넌트에 search-bar 공통 컴포넌트 적용

--- src/components/board/board-search-bar.tsx
+++ src/components/board/board-search-bar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { useState } from 'react'
-import BoardSelectBox from './board-select-box'
+import TextSearchBar from '../media/common/text-search-bar'
 
 const options = [
   { name: 'title', displayString: '제목' },
@@ -12,15 +11,18 @@ export default function BoardSearchBar() {
   const searchParams = useSearchParams()
   const { replace } = useRouter()
   const pathname = usePathname()
+  const defaultQuery = searchParams.get('query') || ''
 
-  const [query, setQuery] = useState(searchParams.get('query') || '')
-  const [type, setType] = useState(options[0])
-
-  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const handleTextSearch = ({
+    query,
+    type,
+  }: {
+    query: string
+    type: string
+  }) => {
     const params = new URLSearchParams(searchParams)
     params.set('page', '1')
-    params.set('type', type.name)
+    params.set('type', type)
     if (query) {
       params.set('query', query)
     } else {
@@ -30,24 +32,10 @@ export default function BoardSearchBar() {
   }
 
   return (
-    <div className="flex gap-[5px] align-middle text-[12px]">
-      <form onSubmit={handleSearch} className="flex items-end gap-2">
-        <BoardSelectBox
-          options={options}
-          onChange={(option) => setType(option)}
-          selected={type}
-        />
-        <input
-          type="text"
-          className="h-[30px] w-[220px] border border-[#9a9a9a] px-[8px]"
-          placeholder="검색어를 입력해주세요"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button className="h-[30px] w-[50px] rounded-none bg-[#333] py-0 text-white">
-          검색
-        </button>
-      </form>
-    </div>
+    <TextSearchBar
+      options={options}
+      defaultQuery={defaultQuery}
+      onSubmit={handleTextSearch}
+    />
   )
 }
