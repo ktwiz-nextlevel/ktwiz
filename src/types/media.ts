63f95fcaa51dd5dbb70fa06f6d8e083ec303c76f
chldmsqls34 <chldmsqls34@gmail.com>
2025-01-06 15:09:28 +0900

    feat: 북마크 crud 기능 추가

--- src/types/media.ts
+++ src/types/media.ts
@@ -42,3 +42,19 @@ export interface Video {
   videoLink: string
   viewCnt: number
 }
+export interface BookmarkEntity {
+  id: number
+  user_id: string
+  video_id: number
+  video_title: string
+  video_thumbnail: string
+  video_reg: number
+}
+export interface BookmarkListItem {
+  id: number
+  userId: string
+  videoId: number
+  videoTitle: string
+  videoThumbnail: string
+  videoReg: number
+}
