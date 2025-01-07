c3764bb10bb515888459710121a0ff0d6bf7e34c
chldmsqls34 <chldmsqls34@gmail.com>
2025-01-07 12:16:39 +0900

    feat: 검색 결과가 존재하지 않을 경우 표시될 UI 추가

--- src/components/media/common/no-data-list.tsx
+++ src/components/media/common/no-data-list.tsx
@@ -2,7 +2,7 @@ export default function NoDataList() {
   return (
     <div className="container mx-auto mt-10">
       <div className="flex h-[500px] w-full flex-col items-center justify-center rounded-lg border border-gray-200">
-        <p className="text-gray-500">등록된 사진이 없습니다.</p>
+        <p className="text-gray-500">검색 내용에 관련된 결과가 없습니다.</p>
       </div>
     </div>
   )
