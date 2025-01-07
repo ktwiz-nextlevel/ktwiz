5fca185ce6a00583cb80be5a8571f4a02285d6b4
KimPuro <puro@protonmail.ch>
2025-01-08 05:08:24 +0900

    feat: iframe 배너 추가

--- src/components/wizpark/stadium-iframe.tsx
+++ src/components/wizpark/stadium-iframe.tsx
@@ -1,13 +1,14 @@
 import './iframe.css'
+import IframeBanner from '@/components/wizpark/iframe-banner'
 function StadiumIframe() {
   return (
-    <div>
+    <div className="hidden lg:block">
+      <IframeBanner />
       <iframe
         src="https://stadium.kimpuro.com"
         width={1100}
         height={636.172}
         title="KTWiz Stadium 3D"
-        className="hidden md:block"
       ></iframe>
     </div>
   )
